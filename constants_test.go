05a5467a4b86a2c04383a501330f6fc4194e0648
Antti Rasinen <antti.rasinen@codento.com>
2012-10-28 19:17:20 +0200

    Add some tests

--- constants_test.go
+++ constants_test.go
@@ -6,11 +6,23 @@ import (
 )
 
 func equals(x, y float64) bool {
-	return math.Abs(x-y) < 1e-20
+	return math.Abs(x-y) < 1e-10
 }
 
-func TestTurns(t *testing.T) {
-	if !equals(360*Degrees, 1*Turn) {
-		t.Fail()
+func Test(t *testing.T) {
+	var tests = []struct {
+		expected, actual float64
+	}{
+		{360 * Degrees, 1 * Turn},
+		{200 * Grads, math.Pi},
+		{math.Sin(Tau / 4), 1},
+	}
+
+	for k, v := range tests {
+		if !equals(v.expected, v.actual) {
+			t.Log("Expected", v.expected)
+			t.Log("Actual", v.actual)
+			t.Error("Test", k, "failed.")
+		}
 	}
 }
